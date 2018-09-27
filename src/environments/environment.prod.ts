8722f75031bca75b1606a29e1d202d27507776e9
SandraND <lainfaith2@gmail.com>
2018-09-27 10:23:29 +0200

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   currencyURL: 'http://www.json-generator.com/api/json/get/bVviQgqKiG?indent=2',
-  transactionURL: 'http://www.json-generator.com/api/json/get/bTzgmQbyqa?indent=2'
+  transactionURL: 'http://www.json-generator.com/api/json/get/bTzgmQbyqa?indent=2',
+  apiURL: 'https://pwabemobile.herokuapp.com/'
 };
