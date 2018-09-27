137baa179ed70c1cdbc9babd75cd7a22328b11ee
SandraND <lainfaith2@gmail.com>
2018-09-27 10:19:07 +0200

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  currencyURL: 'http://www.json-generator.com/api/json/get/bVviQgqKiG?indent=2',
+  transactionURL: 'http://www.json-generator.com/api/json/get/bTzgmQbyqa?indent=2'
 };
