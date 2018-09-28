c2e54d43b247c921d997415fea2b0cb7a0b0d545
SandraND <lainfaith2@gmail.com>
2018-09-28 14:14:04 +0200

    deploy

--- app.js
+++ app.js
@@ -8,27 +8,28 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-const port = process.env.PORT || 4200;
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      const path = 'https://' + req.get('Host') + req.url;
+      return res.redirect(path);
+    }
+    next();
+  };
+};
+
+app.use(forceSSL());
+
+const port = process.env.PORT || 8080;
 app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('running on port ' +port));
 
-// const forceSSL = function () {
-//   return function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       const path = 'https://' + req.get('Host') + req.url;
-//       return res.redirect(path);
-//     }
-//     next();
-//   };
-// };
-
-// app.use(forceSSL());
 
 // app.use(express.static(path.join(__dirname, '/dist/pwabemobile')));
 
